19fc3b22b367d9141aba264c8b625a19767dc875
812jay <yongje812@gmail.com>
2020-12-07 17:24:52 +0900

    #2.1 Getting the Condition Names

--- App.js
+++ App.js
@@ -12,11 +12,20 @@ export default class App extends React.Component {
     isLoading: true,
   };
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp: data.main.temp });
-    console.log(data.main);
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+    console.log(this.state.condition);
   };
   getLocation = async () => {
     try {
@@ -33,7 +42,11 @@ export default class App extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
